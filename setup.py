75958c9429f724ed007a962628348903a2d676b9
Avinash Bhat <avinashbhat49@gmail.com>
2023-11-13 20:51:47 +0100

    code cleanup done,bugs fixed

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='your-package-name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[ ],
+    entry_points={
+        'console_scripts': [],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    url='https://github.com/your-username/your-repo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
